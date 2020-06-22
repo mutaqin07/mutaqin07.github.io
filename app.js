0e635d1ec8f8f853a6958fa574432b3376eb571a
mutaqin07 <mutaqinyes@gmail.com>
2020-06-22 13:26:42 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const cameraView = document.querySelector("#camera--view"),
     cameraTrigger = document.querySelector("#camera--trigger")
 // Access the device camera and stream to cameraView
 function cameraStart() {
+    cameraView.setAttribute('autoplay', '');
+    cameraView.setAttribute('muted', '');
+    cameraView.setAttribute('playsinline', '');
     navigator.mediaDevices
         .getUserMedia(constraints)
         .then(function(stream) {
